078e2f9e39803c672e5bd926986b1e722ddf0169
YayaIniee <yayainiee@gmail.com>
2023-11-17 09:30:43 +0700

    Update home.blade.php

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.guest')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid p-0 m-0">
     <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -30,16 +30,55 @@
     </div>
 </div>
 
-<div class="container mt-5 py-3 px-3" id="about">
-    <div class="row">
-        <div class="col-12 text-center">
-            <h2>Computer Science</h2>
-        </div>
-        <div class="col-4 mx-4">
-            <img src="https://id-media.apjonlinecdn.com/wysiwyg/blog/best-hp-laptops-for-programming/Programmer_Coding_on_Laptop.jpg" class="img-fluid">
-        </div>
-        <div class="col-6">
-            computer science is a multidimensional field that encompasses algorithmic thinking, artificial intelligence, cybersecurity, infrastructure management, and ethical considerations. Its pervasive influence in the digital age is undeniable, and as it continues to advance, it will shape the future in ways we are only beginning to comprehend. This abstract offers a glimpse into the vast and intricate landscape of computer science, revealing the interplay of theory and application that propels innovation and progress in the 21st century.
+<div class="container-fluid m-3">
+    <div class="row justify-content-center">
+        <div class="col-xl-10">
+            <div class="row align-items-center">
+                <h2 class="title text-center py-2">Why Computer Science?</h2>
+                <div class="col-lg-6 text-center">
+                    <img src="https://id-media.apjonlinecdn.com/wysiwyg/blog/best-hp-laptops-for-programming/Programmer_Coding_on_Laptop.jpg" class="img-fluid">
+                </div>
+                <div class="col-lg-6 col-xl-5S">
+                <div class="accordion" id="Cees">
+                    <div class="accordion-item border-0">
+                        <h2 class="accordion-header" id="headingOne">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Accordion Item #1
+                        </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#Cees">
+                        <div class="accordion-body">
+                            <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item border-0">
+                        <h2 class="accordion-header" id="headingTwo">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Accordion Item #2
+                        </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#Cees">
+                        <div class="accordion-body">
+                            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
+                    <div class="accordion-item border-0">
+                        <h2 class="accordion-header" id="headingThree">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Accordion Item #3
+                        </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#Cees">
+                        <div class="accordion-body">
+                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                        </div>
+                        </div>
+                    </div>
+                </div>                
+            </div>
+            </div>
         </div>
     </div>
 </div>
@@ -55,7 +94,6 @@
     <div class="row mt-4 text-dark">
             <div class="col-md-3">
                 <div class="card">
-                    <img src="{{ asset('images/carousel3.jpg') }}" class="card-img-top" alt="Image 1">
                     <div class="card-body">
                         <h5 class="card-title">Abdullah Ammar</h5>
                         <p class="card-text">Lomba Poster Digital ASTRO ( Aksi, Seni, Teknologi, dan Olahraga ) Competition 2018</p>
